ab82f389e1838e86a92f0038180a1ec7742a68d4
Azat201003 <zanelane87@gmail.com>
2023-11-04 13:27:51 +0400

    beautifull

--- nedoChess/nedoChess/nedoChess.cpp
+++ nedoChess/nedoChess/nedoChess.cpp
@@ -1,56 +1,152 @@
 ﻿#include <string>
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-int* hourse(int x, int y) {
+void _clear(int index, vector<vector<int>>* allSteps) {
+	for (int i = index; i < (*allSteps).size(); i++) {
+		(*allSteps).at(i).at(0) = -1;
+	}
+}
 
-	int b[8][2];
+vector<vector<int>> bishop(int x, int y) {
+	vector <vector <int>> allSteps(14, vector<int>(2));
+	int index = 0;
+	for (int x1 = -8; x1 < 8; x1++) {
+		if (x1 != 0) {
+			if (x + x1 < 8 && x + x1 >= 0 && y + x1 < 8 && y + x1 >= 0) {
+				allSteps.at(index).at(0) = x + x1;
+				allSteps.at(index).at(1) = y + x1;
+				index++;
+			}
+
+			if (x + x1 < 8 && x + x1 >= 0 && y - x1 < 8 && y - x1 >= 0) {
+				allSteps.at(index).at(0) = x + x1;
+				allSteps.at(index).at(1) = y - x1;
+				index++;
+			}
+		}
+	}
+	_clear(index, &allSteps);
+
+	return allSteps;
+}
+vector<vector<int>> rook(int x, int y) {
+	vector <vector <int>> allSteps(14, vector<int>(2));
+	int index = 0;
+	for (int x1 = -8; x1 < 8; x1++) {
+		if (x1 != 0) {
+			if (x + x1 < 8 && x + x1 >= 0) {
+				allSteps.at(index).at(0) = x + x1;
+				allSteps.at(index).at(1) = y;
+				index++;
+			}
+
+			if (y + x1 < 8 && y + x1 >= 0) {
+				allSteps.at(index).at(0) = x;
+				allSteps.at(index).at(1) = y + x1;
+				index++;
+			}
+		}
+	}
+	_clear(index, &allSteps);
+
+	return allSteps;
+}
+
+vector<vector<int>> knight(int x, int y) {
+
+	vector<vector<int>> allSteps(8, vector<int>(2));
 	int index = 0;
 	for (int x1 = -2; x1 <= 2; x1++) {
 		for (int y1 = -2; y1 <= 2; y1++) {
-			if (x1 == 0 || y1 == 0 || x1 == y1) {
-				continue;
-			}
-			else {
+			if (x1 != 0 && y1 != 0 && abs(x1) != abs(y1)) {
 				if (x + x1 >= 0 && x + x1 < 8 && y + y1 >= 0 && y + y1 < 8) {
-					b[index][0] = x1 + x;
-					b[index][1] = y + y1;
+					allSteps.at(index).at(0) = x1 + x;
+					allSteps.at(index).at(1) = y + y1;
 					index++;
 				}
 			}
 		}
 	}
-	return *b;
+	_clear(index, &allSteps);
+
+	return allSteps;
 }
 
-int* Pe(int x, int y) {
 
-	int b[2][2];
-	int y1;
-	int index = 0;
-	b[0][0] = x;
-	b[0][1] = y + 1;
+
+vector<vector<int>> pawn(int x, int y) {
+
+	vector<vector<int>> allSteps(2, vector<int>(2));
+	allSteps.at(1).at(0) = -1;
+
+	allSteps.at(0).at(0) = x;
+	allSteps.at(0).at(1) = y + 1;
 
 	if (y == 1) {
-		b[1][0] = x;
-		b[1][1] = y + 1;
+		allSteps.at(1).at(0) = x;
+		allSteps.at(1).at(1) = y + 2;
 	}
-	return *b;
+	return allSteps;
 }
 
+enum chessPieces {
+	Bishop = 0,
+	Rook = 1,
+	Pawn = 2,
+	Knight = 3,
+};
 
+void printOfSteps(int x, int y, chessPieces piece) {
+	vector<vector<int>> allSteps;
+	switch (piece)
+	{
+	case chessPieces::Bishop:
+		cout << " \n\nслон:";
+		allSteps = bishop(x, y);
+		break;
+	case chessPieces::Knight:
+		cout << " \n\nконь:";
+		allSteps = knight(x, y);
+		break;
+	case chessPieces::Pawn:
+		cout << " \n\nпешка:";
+		allSteps = pawn(x, y);
+		break;
+	case chessPieces::Rook:
+		cout << " \n\nладья:";
+		allSteps = rook(x, y);
+		break;
+	default:
+		cout << "error, no this index in 'chessPieces'";
+		break;
+	}
+	for (auto i : allSteps) {
+		if (i.at(0) == -1) {
+			continue;
+		}
+		cout << "\n\nx: " << i.at(0) << "\ny: " << i.at(1);
+	}
+}
 
 int main()
 {
 	setlocale(LC_ALL, "RUSSIAN");
-	int x,y;
-	
-	for (auto i :hourse(x,y)) {
-		if (i[0] == -858993460) {
+	int y, x;
+	while (true) {
+
+		cin >> x >> y;
+		if (x < 8 && x >= 0 && y < 8 && y >= 0) {
 			break;
 		}
-		cout << "x: " << i[0] << "y: " << i[1];
+		else {
+			cout << "ti tupoi normalno pishi\n";
+		}
 	}
-		
+	printOfSteps(x, y, chessPieces::Bishop);
+	printOfSteps(x, y, chessPieces::Knight);
+	printOfSteps(x, y, chessPieces::Pawn);
+	printOfSteps(x, y, chessPieces::Rook);
 }
-
